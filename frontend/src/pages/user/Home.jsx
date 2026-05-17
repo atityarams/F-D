45ac904535a798a6749eac95ecb1a2964c916334
ATITYA RAM S <atityarams.24cse@kongu.edu>
2026-05-17 14:58:12 +0530

    Update Home.jsx

--- frontend/src/pages/user/Home.jsx
+++ frontend/src/pages/user/Home.jsx
@@ -4,7 +4,8 @@ function Home() {
 
         <div>
 
-            <h1>Welcome to Aaswitha's VaangaJuicePodalam Series</h1>
+            <h1>Welcome to Aaswitha's VaangaJuicePodalam 
+                Series</h1>
 
             <p>
                 Explore hard working, women who is afraid of dark's[dog's] products.
