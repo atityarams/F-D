74bdb93513fb191a333038e91ad11e352962e04b
ATITYA RAM S <atityarams.24cse@kongu.edu>
2026-05-17 15:08:35 +0530

    Update Home.jsx

--- frontend/src/pages/user/Home.jsx
+++ frontend/src/pages/user/Home.jsx
@@ -2,13 +2,26 @@ function Home() {
 
     return (
 
-        <div>
+        <div style={{ padding: '20px' }}>
 
-            <h1>Welcome to Aaswitha's VaangaJuicePodalam 
+            <h1>Aaswitha's VaangaJuicePodalam 
                 Series</h1>
 
             <p>
-                Explore hard working, women who is afraid of dark's[dog's] products.
+                Explore hard working, women.
+
+                Venu -> Amaravthi
+
+                Fee (1 month) -> 1000 
+
+                Prerequisition:
+                            Person must known to boil water
+                            Only Sweet Lime should be bought based on user requirements
+                            Glassware should by brought by them for presenting
+                            **ONLY 5 Star Rating must be given in FeedBack**
+                What does this class provides:
+                **Patients**
+                **Professional** **Fruit** **Squishing** **method**
             </p>
 
         </div>
